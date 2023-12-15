e25032d40f08951a6c727c51fcea89f05ce8ad4f
QuocDL <quoclcph18659@gmail.com>
2023-12-15 09:06:45 +0700

    NODE JS

---
+++ src/middlewares/checkPermission.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv";
+import { verifyToken } from "../utils/verifyToken";
+import User from "../models/User";
+dotenv.config();
+
+export const checkPermission = async (req, res, next) => {
+  try {
+    const token = req.headers?.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({
+        message: "Chưa đăng nhập!",
+      });
+    }
+    const user = verifyToken(token);
+
+    // Kiểm tra xem user có quyền hạn này hay không?
+    if (user.role === "admin") {
+      return res.status(403).json({
+        message: "Bạn ko phải là người có quyền này!",
+      });
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      name: error.name || "Error",
+      message: error.message || "Server error!",
+    });
+  }
+};
---
+++ src/models/Product.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    image: String,
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: String,
+    categoryID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("Product", productSchema);
